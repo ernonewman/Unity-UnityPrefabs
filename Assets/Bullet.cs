cad1fc531e6a3b12ee128e697d15536383eb77df
Erno Newman <ernonewman@gmail.com>
2020-09-09 19:16:04 +0100

    "Build a Battle Royale and an RPG in Unity and Blender!" Udemy course Section 4 Part 20 and 21: Introduction to Prefabs I forgot to save the blank project

---
+++ Assets/Bullet.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float ShootingForce = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody>().AddForce(Vector3.forward * ShootingForce);
+    }
+}
