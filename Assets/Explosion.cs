f5a878fab304cb70563debd99f3d25ed02fc7dfc
Erno Newman <ernonewman@gmail.com>
2020-09-09 19:55:35 +0100

    Part 23: Explosion effects

---
+++ Assets/Explosion.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    public GameObject[] particlePrefabs;
+    public int AmountOfParticles = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < AmountOfParticles; i++)
+        {
+            GameObject particlePrefab = Instantiate(particlePrefabs[Random.Range(0, particlePrefabs.Length)]);
+            particlePrefab.transform.position = transform.position;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
---
+++ Assets/ExplosionParticle.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionParticle : MonoBehaviour
+{
+    public float ExplosionForce = 150f;
+    public float lifetime = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 randomDirection =
+            new Vector3(
+                Random.Range(-1f, 1f),
+                Random.Range(-1f, 1f),
+                Random.Range(-1f, 1f));
+
+        var randomForce = Random.Range(0f, ExplosionForce);
+
+        GetComponent<Rigidbody>().AddForce(randomDirection.normalized * randomForce);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lifetime -= Time.deltaTime;
+
+        if (lifetime <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
